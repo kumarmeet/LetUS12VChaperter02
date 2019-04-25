59f84e4dcb984cdc209dc27e5c6d7d5f4e33c400
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2019-04-25 23:25:18 +0530

    Add files via upload

--- IfElsePointLiesInsideTheCircle/main.c
+++ IfElsePointLiesInsideTheCircle/main.c
@@ -3,9 +3,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    float x, y, radius, d;
+
+    printf("Enter the circle co-rodinates (x, y): ");
+    scanf("%f %f", &x, &y);
+
+    printf("Enter the circle radius: ");
+    scanf("%f", &radius);
+
+    if(radius  > sqrt( pow (x, 2)  +  pow(y, 2)))   {
+        printf("\n\nThe pints lie inside the circle.");
+
+    }
+    else if(radius ==  sqrt(pow (x, 2)  +  pow (y, 2)))   {
+        printf("\n\nThe points lie on the circle.");
+    }
+    else   {
+        printf("\n\nThe points lie outside the circle.");
+    }
+
     return 0;
 }
